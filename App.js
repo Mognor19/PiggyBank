7d26235503199007a60252cc0393fa0af567119e
Mognor19 <antonyflores779@gmail.com>
2021-03-01 19:21:19 -0600

    Stack to Tab navigation

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Login from './src/screen/Login';
+import ExpenseList from './src/screen/ExpenseList'
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Login} />
+      <Tab.Screen name="Settings" component={ExpenseList} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Start" component={Login} />
+      <Stack.Screen name="Home" component={MyTabs} />
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
