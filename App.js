5cce75298bb1d329ed6d4fa9e5fbee54098c829b
akumanam <nahumlop17@gmail.com>
2021-03-05 19:49:59 -0600

    Borrador

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ function MyTabs() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Start" component={Login} />
       <Stack.Screen name="Home" component={MyTabs} />
       <Stack.Screen name="Recover" component={Recover} />
--- src/screen/Recover.js
+++ src/screen/Recover.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions} from 'react-native';
+import { colors } from 'react-native-elements';
+import { TextInput } from 'react-native-gesture-handler';
 const {width, height} = Dimensions.get("screen");
+import theme from '../components/theme/index'
 
 const Recover = ({ navigation }) => {
+    const [value, onChangeText] = React.useState('Vieja Contraseña');
     return (
         <View style={styles.container}>
-            <Text>Test</Text>
+            <TextInput
+                style={{borderColor: theme.colors.black, borderWidth: 1 }}
+                onChangeText={text => onChangeText(text)}
+                value={value}
+            />
+
             
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('Recover')}
+            >
+                <Text style={styles.Text}>Actualizar</Text>
+            </TouchableOpacity>            
         </View>
     )
 }
@@ -16,13 +31,17 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent:'center',
         alignItems: 'center',
+        backgroundColor: theme.colors.grey,
     },
     button:{
         alignItems:'center',
-        width: width * 0.8,
+        width: width * 0.5,
         padding: 5,
-        backgroundColor:'grey'
+        backgroundColor:theme.colors.blue,
     },
+    Text:{
+        color: theme.colors.gold,
+    }
 });
 
 export default Recover;
