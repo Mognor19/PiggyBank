e3f7a739d8580f02b8b2db59111627a5d8b36c1c
akumanam <nahumlop17@gmail.com>
2021-03-06 18:35:39 -0600

    Borrador 1

--- src/screen/Recover.js
+++ src/screen/Recover.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions} from 'react-native';
-import { colors } from 'react-native-elements';
 import { TextInput } from 'react-native-gesture-handler';
 const {width, height} = Dimensions.get("screen");
 import theme from '../components/theme/index'
 
 const Recover = ({ navigation }) => {
-    const [value, onChangeText] = React.useState('Vieja Contraseña');
+    const value = React.useState('');
+    const value1 = React.useState('');
+    const value2 = React.useState('');
     return (
         <View style={styles.container}>
-            <TextInput
-                style={{borderColor: theme.colors.black, borderWidth: 1 }}
-                onChangeText={text => onChangeText(text)}
+            
+            <TextInput placeholder={' Vieja Contraseña'}
+            
+                style={styles.textinput}
                 value={value}
             />
 
+            <TextInput placeholder={' Nueva Contrseña'} 
+                style={styles.textinput}
+                value={value1}
+            />
+
+            <TextInput placeholder={' Confirmar Contraseña'} 
+                style={styles.textinput}
+                value={value2}
+            />
             
             <TouchableOpacity
                 style={styles.button}
@@ -35,12 +46,25 @@ const styles = StyleSheet.create({
     },
     button:{
         alignItems:'center',
-        width: width * 0.5,
+        width: width * 0.4,
+        height: width *0.075,
         padding: 5,
         backgroundColor:theme.colors.blue,
     },
     Text:{
         color: theme.colors.gold,
+    },
+    textinput:{
+        borderColor: theme.colors.black,
+        borderWidth: 1,
+        color: theme.colors.black,
+        width: width * 0.8,
+        margin: 10,
+    },
+    logo:{
+        width: width * 0.5,
+        height: width * 0.5,
+        margin: 10,
     }
 });
 
